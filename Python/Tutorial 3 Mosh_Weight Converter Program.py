3b8103cf144e1ac671e6b796fffc2c9197bbc25d
Mehrol911 <80707348+Mehrol911@users.noreply.github.com>
2021-11-11 18:36:53 +0500

    Add files via upload

---
+++ Python/Tutorial 3 Mosh_Weight Converter Program.py
@@ -0,0 +1,11 @@
+#weight program converter
+Weight = int(input("Weight: "))
+unit = input("(L) bs or (K)g: ")
+if unit.upper() =="L": 
+   converted = Weight * 0.45
+   print(f"You are {converted} kilos")
+elif unit.upper() =="K":
+    converted = Weight // 0.45
+    print(f"You are {converted} pounds")
+else: 
+    print("Write proper value")   
