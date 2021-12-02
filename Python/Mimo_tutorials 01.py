800a143d70cea35d3da6042eced90cbf852a4554
Mehrol911 <80707348+Mehrol911@users.noreply.github.com>
2021-12-02 07:44:17 +0500

    Update and rename Mimo_tutorials 1 to Mimo_tutorials 01.py

---
+++ Python/Mimo_tutorials 01.py
@@ -0,0 +1,6 @@
+#formatted strings   02.12.2021
+print(f"{2} new messages) // adding a string to a number
+new = 5
+status = f"{new} new messages"
+print(status)
+----------------------------------------------------------
