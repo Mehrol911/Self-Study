3222c9f11de4b92c978735467bdecde82abc3f13
Mehrol911 <80707348+Mehrol911@users.noreply.github.com>
2021-12-05 11:15:49 +0500

    Delete Mimo_tutorials 01.py

--- Python/Mimo_tutorials 01.py
+++
@@ -1,6 +0,0 @@
-#formatted strings   02.12.2021
-print(f"{2} new messages) // adding a string to a number
-new = 5
-status = f"{new} new messages"
-print(status)
-----------------------------------------------------------
