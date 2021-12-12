59b0ed46b032044effa6f8b6dab6053c7092b378
Mehrol911 <80707348+Mehrol911@users.noreply.github.com>
2021-12-12 11:04:24 +0500

    Create Slide 12 - GUI.java
    Preperation final

---
+++ Java/Slide 12 - GUI.java
@@ -0,0 +1,18 @@
+// Create a button with text OK 
+JButton jbtOK = new JButton("OK"); 
+ 
+// Create a label with text "Enter your name: "
+JLabel jlblName = new JLabel("Enter your name: "); 
+ 
+// Create a text field with text "Type Name Here"
+JTextField jtfName = new JTextField("Type Name Here"); 
+ 
+// Create a check box with text bold
+JCheckBox jchkBold = new JCheckBox("Bold"); 
+ 
+// Create a radio button with text red
+JRadioButton jrbRed = new JRadioButton("Red"); 
+ 
+// Create a combo box with choices red, green, and blue
+JComboBox jcboColor = new JComboBox(new String[]{"Red", 
+  "Green", "Blue"}); 
