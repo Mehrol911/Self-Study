8e39501471984a81706f8cbbf00525f204ee31fd
Mehrol911 <80707348+Mehrol911@users.noreply.github.com>
2021-12-18 09:34:09 +0500

    Create 1. Database.java

---
+++ Java/1. Database.java
@@ -0,0 +1,21 @@
+/*** 1. Consider following database design (any database product i.e. MySql, Sqlite, MsSQL,,,,,,)
+Database Name : MobileDB
+Table Name : Mobile ***/
+import java.sql.*;
+public class JdbcSelectTest_insert
+{
+public static void main(String[] args) throws SQLException
+{
+Connection conn = DriverManager.getConnection ("jdbc:mysql://localhost/MobileDB?autoReconnect=true&useSSL=false", "root", "abhijit12345");
+PreparedStatement stmt=conn.prepareStatement("insert into mobile values(?,?,?)");
+stmt.setInt(1,3);
+stmt.setString(2,"Redmi POCO");
+stmt.setInt(3,250);
+int i=stmt.executeUpdate();
+conn.close();
+}
+}
+| First Header  | Second Header |
+| ------------- | ------------- |
+| Content Cell  | Content Cell  |
+| Content Cell  | Content Cell  |
