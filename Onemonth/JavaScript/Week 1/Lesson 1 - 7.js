2beef5475ffd909fa90d999c069972b798a98e9c
Mehrol911 <80707348+Mehrol911@users.noreply.github.com>
2022-01-01 15:59:27 +0500

    Update Lesson 1 - 7.js

--- Onemonth/JavaScript/Week 1/Lesson 1 - 7.js
+++ Onemonth/JavaScript/Week 1/Lesson 1 - 7.js
@@ -1,6 +1,5 @@
 /* this is an event */ 
 /* alert("I'm annoying") w1 L2 */    /*comments w1 L3*/
-<script>
 var firstname = "Christopher"; // Variables - w1 L5
 var lastname = "Castig";
 var fullname = firstname + " " + lastname;  // Concatenation - w1 L6
@@ -12,4 +11,3 @@ var c = a + b;
 document.write("<h1>" + c + "</h1>"); /*Printing - w1 L4*/
 document.write("<h1>" + c + "</h1>");
 document.write("<h1>" + c + "</h1>"); 
-</script>
