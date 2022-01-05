e0eadacb195ddf2b7a57b3dab5c3ccc07e8da2b0
Mehrol911 <80707348+Mehrol911@users.noreply.github.com>
2022-01-05 14:27:22 +0500

    Add files via upload

---
+++ Python/Mekhrol_Bazarov_HW_05.01.2022.py
@@ -0,0 +1,19 @@
+#Mekhrol_Bazarov
+#Task 1 
+Dictionary = {
+    0: 10,
+    1: 20,
+}
+Dictionary[2]=30
+print(Dictionary)
+
+#Task 2
+dict1 = {1:10, 2:20}
+dict2 = {3:30, 4:40}
+dict3 = {5:50, 6:60}
+birlashgan = {**dict1, **dict2, **dict3}
+print(birlashgan)
+
+#Task3
+x = birlashgan.keys()
+print(x)
